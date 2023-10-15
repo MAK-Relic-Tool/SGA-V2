0c3fc9f12c92d7a1a05e523abe95266ff969daea
Marcus Kertesz <ModernMAK@Hotmail.com>
2023-10-15 00:19:26 -0700

    Bump to 1.1.0

--- src/relic/sga/v2/__init__.py
+++ src/relic/sga/v2/__init__.py
@@ -7,7 +7,7 @@ from relic.sga.v2.definitions import (
 
 from relic.sga.v2.serialization import essence_fs_serializer as EssenceFSHandler
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 __all__ = [
     "EssenceFSHandler",
